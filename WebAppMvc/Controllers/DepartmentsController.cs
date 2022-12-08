0cf0f236bd0be4ba09f8a035ef48655cc28e2efa
EnzoBellini <bellinienzo0@gmail.com>
2022-12-08 20:50:43 -0300

    Created my first View and Controller in MVC

---
+++ WebAppMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppMvc.Models;
+
+namespace WebAppMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> lst = new List<Department>();
+            lst.Add(new Department
+            {
+                Id = 1,
+                Name = "Eletronics"
+            });
+
+            lst.Add(new Department
+            {
+                Id = 2,
+                Name = "Fashion"
+            });
+
+            return View(lst);
+        }
+    }
+}
