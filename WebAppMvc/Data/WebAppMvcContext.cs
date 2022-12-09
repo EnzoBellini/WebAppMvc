a78956578782cb62e8a320d024ef135eb7e71026
EnzoBellini <bellinienzo0@gmail.com>
2022-12-08 21:09:59 -0300

    CRUD scaffolding

---
+++ WebAppMvc/Data/WebAppMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppMvc.Models;
+
+namespace WebAppMvc.Data
+{
+    public class WebAppMvcContext : DbContext
+    {
+        public WebAppMvcContext (DbContextOptions<WebAppMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppMvc.Models.Department> Department { get; set; } = default!;
+    }
+}
--- WebAppMvc/Program.cs
+++ WebAppMvc/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppMvc.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebAppMvcContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAppMvcContext") ?? throw new InvalidOperationException("Connection string 'WebAppMvcContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
